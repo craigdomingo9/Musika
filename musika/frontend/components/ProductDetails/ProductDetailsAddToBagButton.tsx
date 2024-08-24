b4335b640b9b090ef27d4179d72fc86725bd1884
Craig Domingo <craigdomingo9@gmail.com>
2024-08-24 20:50:03 +0200

    Commit message

--- musika/frontend/components/ProductDetails/ProductDetailsAddToBagButton.tsx
+++ musika/frontend/components/ProductDetails/ProductDetailsAddToBagButton.tsx
@@ -2,23 +2,32 @@
 import useBagStore from "@/stores/BagStore"
 import { Button } from "../ui/button"
 import useCurrentProductStore from "@/stores/CurrentProductStore"
+import { useToast } from "@/components/ui/use-toast"
 
 function ProductDetailsAddToBagButton() {
+  const { toast } = useToast()
 
   const addProductToStore = useBagStore((state) => state.addItem)
-  const bag = useBagStore((state) => state.items)
 
   const current_product = useCurrentProductStore((state) => state.currentProduct);
 
   const addToBag = () => {
     addProductToStore(current_product);
-  }
 
+    toast({
+      variant: "success",
+      description: "Product has been added to Bag",
+      duration: 1500,
+    })
+
+  }
 
 
   return (
     <div className="w-1/2 p-2">
-        <Button onClick={addToBag} className="w-full h-16 bg-amber-600 opacity-95">Add to Bag</Button>
+        <Button onClick={addToBag} className="w-full h-16 bg-color-btn hover:bg-color-btn">
+          Add to Bag
+        </Button>
     </div>
   )
 }
